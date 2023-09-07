8127b331b2c04975676ab179592aaa74341eb278
Va1ue-0268 <la1ure11nce@gmail.com>
2023-09-07 21:52:50 +0800

    update

--- 其他自用脚本/login.py
+++ 其他自用脚本/login.py
@@ -1,24 +1,45 @@
 import requests
 
-url = 'http://10.99.99.99:8010/cgi-bin/webauth/ajax_webauth'
+url0 = 'http://47.98.217.39/lfradius/libs/portal/unify/portal.php/login/cmcc_login'
+url1 = 'http://47.98.217.39/lfradius/libs/portal/unify/portal.php/login/cmcc_login_result/'
+url2 = 'http://47.98.217.39/lfradius/libs/portal/unify/portal.php/login/success/'
 header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36',
-    'Referer': 'http://47.98.217.40/',
+    'Referer': 'http://47.98.217.39/',
     }
 
 # 参数自己改
 
-data = {
-    "action": "login", 
-    "user": "账号", 
-    "pwd": "密码", 
-    "usrmac": "设备mac", 
-    "ip": "设备ip", 
-    "success": "http://10.99.99.99:8080/webauth/success.html#", 
-    "fail": "http://10.99.99.99:8080/webauth/fail.html#", 
-    "clear": "http://10.99.99.99:8080/webauth/clear.html#"
+username = 
+password = 
+
+data0 = {
+    "usrname": username,
+    "passwd": password,
+    "treaty": "on",
+    "nasid": "3",
+    "offline": "0",
+    "protal_version": "1",
+    "protal_papchap": "pap",
+    "usrmac": "48:51:c5:ab:d4:5c",
+    "usrip": "10.117.11.4",
+    "basip": "172.17.127.254",
+    "success": "http://47.98.217.39/lfradius/libs/portal/unify/portal.php/login/success/",
+    "fail": "http://47.98.217.39/lfradius/libs/portal/unify/portal.php/login/fail"
 }
 
-mes = requests.post(url, headers=header, data=data)
+mes = requests.post(url0, headers=header, data=data0)
 # 返回的结果
-print(str(1)+str(mes.status_code)+':'+str(mes.text))
+value = str(mes.text).split('value="')[1].split('"')[0]
+
+data1 = {
+    "cmcc_login_value": value
+}
+
+mes = requests.post(url0, headers=header, data=data1)
+print(str(mes.text))
+mes = requests.post(url1, headers=header, data=data1)
+
+print(str(mes.text))
+
+# mes = requests.get(url2)
